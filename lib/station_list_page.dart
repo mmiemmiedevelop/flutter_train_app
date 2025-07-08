12ce035bad934f8db4312a25fa69dec3fb4762a6
mmie <samlee.develop@gmail.com>
2025-07-08 16:26:51 +0900

    remove: 안쓰는 import 제거 외 추가 문법수정 - Key? key > super.key(flutter 버전따라서 문법바꿈)

--- lib/station_list_page.dart
+++ lib/station_list_page.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'constants/station_list.dart';
-import 'constants/lang.dart';
 
 class StationListPage extends StatelessWidget {
   final String title;
-  const StationListPage({Key? key, required this.title}) : super(key: key);
+  const StationListPage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
