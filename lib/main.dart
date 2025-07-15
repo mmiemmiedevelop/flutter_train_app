a462dda9a2d7bb2812ee86d5de0ddff9e0b6f94d
mmie <samlee.develop@gmail.com>
2025-07-15 17:50:12 +0900

    style: 라이트모드 기본으로 변경

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      themeMode: ThemeMode.dark,
+      themeMode: ThemeMode.light,
       theme: ThemeData(
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(
