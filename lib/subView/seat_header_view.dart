98697153d975830c9602679fc90761afdd11d801
mmie <samlee.develop@gmail.com>
2025-07-15 17:49:40 +0900

    fix: 네글자 역 짤림현상 해결

--- lib/subView/seat_header_view.dart
+++ lib/subView/seat_header_view.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 class SeatHeaderView extends StatelessWidget {
-  const SeatHeaderView({super.key, required this.departureStation, required this.arrivalStation});
+  const SeatHeaderView({
+    super.key,
+    required this.departureStation,
+    required this.arrivalStation,
+  });
   final String departureStation;
   final String arrivalStation;
 
@@ -11,22 +15,32 @@ class SeatHeaderView extends StatelessWidget {
       children: [
         Spacer(),
         Expanded(
+          flex: 3,
           child: Text(
+            textAlign: TextAlign.center,
             departureStation,
-            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.purple),
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: Colors.purple,
+            ),
+            maxLines: 1,
           ),
         ),
         Spacer(),
         Expanded(child: Icon(Icons.arrow_circle_right_outlined, size: 30)),
         Spacer(),
         Expanded(
+          flex: 3,
           child: Text(
+            textAlign: TextAlign.center,
             arrivalStation,
             style: TextStyle(
               fontSize: 30,
               fontWeight: FontWeight.bold,
               color: Colors.purple,
             ),
+            maxLines: 1,
           ),
         ),
         Spacer(),
